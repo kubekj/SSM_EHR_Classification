a0b811637406ebde48420539ec18e2e4b7316018
kubekj <kuba.jedraszka@gmail.com>
2024-11-16 20:26:50 +0100

    Went simple as suggested in cloned repo

--- hpc_jobs/evaluate_grud.sh
+++ hpc_jobs/evaluate_grud.sh
@@ -10,18 +10,16 @@
 #BSUB -W 24:00
 #BSUB -N
 
-cd /dtu/blackhole/10/203216/SSM_EHR_Classification
-
 # Load modules and setup environment
 module load python3/3.9.19
 module load cuda/11.8
 module load cudnn/v8.8.0-prod-cuda-11.X
 python -m venv ./venv
-./venv/bin/activate
+source ./venv/bin/activate
 pip install -r requirements.txt
 pip install torch_scatter --extra-index-url https://data.pyg.org/whl/torch-2.2.0+cu118.html
 
-python scripts/cli.py \
+python cli.py \
     --output_path=grud_output \
     --model_type=grud \
     --epochs=100 \
