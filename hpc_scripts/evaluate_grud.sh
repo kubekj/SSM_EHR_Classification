8e0ca4a7fee7b2eb9a6f30feb65ee0a77387cfa8
kubekj <kuba.jedraszka@gmail.com>
2024-11-16 20:51:56 +0100

    Folder adjustments

--- hpc_scripts/evaluate_grud.sh
+++ hpc_scripts/evaluate_grud.sh
@@ -14,12 +14,21 @@
 module load python3/3.9.19
 module load cuda/11.8
 module load cudnn/v8.8.0-prod-cuda-11.X
-python -m venv ./venv
-source ./venv/bin/activate
-pip install -r requirements.txt
+
+# Create venv if it doesn't exist
+if [ ! -d "../.venv" ]; then
+    echo "Creating new virtual environment..."
+    python -m venv ../.venv
+fi
+
+source ../.venv/bin/activate
+
+# Install requirements
+echo "Installing requirements..."
+pip install -r ../requirements.txt
 pip install torch_scatter --extra-index-url https://data.pyg.org/whl/torch-2.2.0+cu118.html
 
-python cli.py \
+python ../scripts/cli.py \
     --output_path=grud_output \
     --model_type=grud \
     --epochs=100 \
