78848626c8058ca85ad4f041b4af5c265f6eeabf
kubekj <kuba.jedraszka@gmail.com>
2024-11-16 19:43:48 +0100

    Replaced the usage of setup.sh as it wouldn't find the file while running as a planned job

--- hpc_jobs/evaluate_transformer.sh
+++ hpc_jobs/evaluate_transformer.sh
@@ -10,9 +10,20 @@
 #BSUB -W 24:00
 #BSUB -N
 
-# Run setup script
-. setup.sh
+module load python3/3.9.19
+module load cuda/11.8
+module load cudnn/v8.8.0-prod-cuda-11.X
 
+if [ ! -d "venv" ]; then
+    python -m venv ./venv
+fi
+
+source ./venv/bin/activate
+
+pip install -r requirements.txt
+pip install torch_scatter --extra-index-url https://data.pyg.org/whl/torch-2.2.0+cu118.html
+
+# Run the evaluation
 python cli.py \
     --output_path=transformer_output \
     --model_type=transformer \
