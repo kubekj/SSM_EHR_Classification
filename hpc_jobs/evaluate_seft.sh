cba902f59d145066a36e03614f17d43e154ede3a
kubekj <kuba.jedraszka@gmail.com>
2024-11-16 19:59:06 +0100

    quick fixes

--- hpc_jobs/evaluate_seft.sh
+++ hpc_jobs/evaluate_seft.sh
@@ -10,18 +10,10 @@
 #BSUB -W 24:00
 #BSUB -N
 
-module load python3/3.9.19
-module load cuda/11.8
-module load cudnn/v8.8.0-prod-cuda-11.X
-
-python -m venv ./venv
-./venv/bin/activate
-
-pip install -r requirements.txt
-pip install torch_scatter --extra-index-url https://data.pyg.org/whl/torch-2.2.0+cu118.html
+source ./hpc_jobs/setup.sh
 
 # Run the evaluation
-python cli.py \
+python ./scripts/cli.py \
     --output_path=seft_output \
     --model_type=seft \
     --epochs=100 \
